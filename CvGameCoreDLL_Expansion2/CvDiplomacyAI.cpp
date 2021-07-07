030220bd4b19036d1d7494e63acc61be3c0ea6bd
RecursiveVision <66801010+RecursiveVision@users.noreply.github.com>
2021-07-07 16:56:59 -0400

    Update CvDiplomacyAI.cpp

--- CvGameCoreDLL_Expansion2/CvDiplomacyAI.cpp
+++ CvGameCoreDLL_Expansion2/CvDiplomacyAI.cpp
@@ -24902,11 +24902,11 @@ void CvDiplomacyAI::DoContactMinorCivs()
 			}
 
 			CvArea* pMinorArea = GC.getMap().getArea(pMinorCapital->getArea());
-			int iMajorCapitalsInArea = 0;
 			if (pMinorArea)
 			{
 				// Do we have a city here?
 				bool bPresenceInArea = pMinorArea->getCitiesPerPlayer(GetID()) > 0;
+				int iMajorCapitalsInArea = 0;
 
 				// Does another major civ have their capital on the same landmass? (must be revealed)
 				for (int iMajorRivalLoop = 0; iMajorRivalLoop < MAX_MAJOR_CIVS; iMajorRivalLoop++)
